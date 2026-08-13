ab49e422a848dfb4f876ca6a009ab841df496b72
ivansvami <151914114+ivansvami@users.noreply.github.com>
2026-08-13 15:11:17 +0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -66,6 +66,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   document.addEventListener('click', function (e) {
+    var mediaClick = e.target.closest('.card:not(.card-youtube) .card-media');
+    if (mediaClick) {
+      var btn = mediaClick.closest('.card').querySelector('.js-post-link');
+      if (btn) {
+        e.preventDefault();
+        openPostModal(btn.dataset.shortid, true);
+        return;
+      }
+    }
     var link = e.target.closest('.js-post-link');
     if (link) {
       e.preventDefault();
